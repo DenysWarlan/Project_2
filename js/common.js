a83d96d6184660eae8fbde0acb4ca635db91cc97
Denys <denis.varlan95@gmail.com>
2018-05-23 08:55:14 +0200

    mobile first

--- js/common.js
+++ js/common.js
@@ -1,16 +1,18 @@
 ;(function ($) {
     $(document).ready(function () {
-        $('.menu-trigger').click(function () {
+        $('.menu-trigger').on('click', function () {
             var ui = $('.menu-trigger'),
                 nav = $('nav ul');
-            nav.slideToggle(768);
-            nav.css('display', 'flex');
+            nav.slideToggle(500);
             ui.toggleClass('open-menu');
             if (ui.hasClass('open-menu')) {
+                nav.css('display', 'flex');
                 $('nav').css('height', '100vh');
             }
             else {
                 $('nav').css('height', '0');
+                nav.css('display', 'none');
+
             }
         });
         $(window).resize(function () {
